906360ab672e0e40607f2230fe3d70ec418d9555
HARISH2K5 <harishr.cs23@gail.com>
2025-09-12 12:13:01 +0530

    Landing Page

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,8 +8,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="min-h-screen bg-gray-50">
         <Providers>
-          <Navbar />
-          <main className="p-4">{children}</main>
+          {/* <Navbar /> */}
+          <main className="">{children}</main>
         </Providers>
       </body>
     </html>
